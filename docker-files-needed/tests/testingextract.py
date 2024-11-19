27e7e07cdad6cd04995a9ddb7eabc02e3bf5f3e5
Gavind. <114811617+ggvndd@users.noreply.github.com>
2024-11-19 10:16:13 +0700

    Update testingextract.py

--- docker-files-needed/tests/testingextract.py
+++ docker-files-needed/tests/testingextract.py
@@ -1,9 +1,9 @@
 from etl.extract import fetch_twitter_trends, fetch_google_trends
 def test_fetch_twitter_trends():
-    API_KEY = 'qvX4Zq69gxYO9bmPpcE2HBzWR'
-    API_KEY_SECRET = 'CNhVsfWIH8wkB7xNMHXmhQVlmUrSm8ruO3p8XpUBKRApFzhR7k'
-    ACCESS_TOKEN = '1081790378729066497-ISlmB5WusykCcDjwftd3TtfaLTsKiV'
-    ACCESS_TOKEN_SECRET = '6yYU43aRCmnliSUOymRXFKFwVSiDZOzOMtLKZNg8NzHtw'
+    API_KEY = 'API_KEY'
+    API_KEY_SECRET = 'API_KEY_SECRET'
+    ACCESS_TOKEN = 'ACCESS_TOKEN'
+    ACCESS_TOKEN_SECRET = 'ACCESS_TOKEN_SECRET'
     #Bearer = AAAAAAAAAAAAAAAAAAAAANTxwwEAAAAAc5tt0dum2yxXSYmjCDSbPXFXR0Q%3DoJ2npeXQpLspUxUSFhXMViy4t7cmadXjSRxIqu0XETTGG1IcIx
     twitter_trends = fetch_twitter_trends(
         API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
@@ -19,10 +19,10 @@ def test_fetch_google_trends():
     assert "date" in google_trends.columns, "'date' column should be present"
 
 if __name__ == "__main__":
-    API_KEY = 'qvX4Zq69gxYO9bmPpcE2HBzWR'
-    API_KEY_SECRET = 'CNhVsfWIH8wkB7xNMHXmhQVlmUrSm8ruO3p8XpUBKRApFzhR7k'
-    ACCESS_TOKEN = '1081790378729066497-ISlmB5WusykCcDjwftd3TtfaLTsKiV'
-    ACCESS_TOKEN_SECRET = '6yYU43aRCmnliSUOymRXFKFwVSiDZOzOMtLKZNg8NzHtw'
+    API_KEY = 'API_KEY'
+    API_KEY_SECRET = 'API_KEY_SECRET'
+    ACCESS_TOKEN = 'ACCESS_TOKEN'
+    ACCESS_TOKEN_SECRET = 'ACCESS_TOKEN_SECRET'
     twitter_trends = fetch_twitter_trends(API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     print("Twitter Trends:", twitter_trends)
 
